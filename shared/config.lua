8edf41da220a19189119f6cb090df2fb1b5ed503
Infinity Development <96385278+infinitydevelopment14@users.noreply.github.com>
2022-05-08 23:12:47 +0200

    Delete shared directory

--- shared/config.lua
+++
@@ -1,6 +0,0 @@
-Config = {}
-
-Config.Hours = 24 -- how many hours players have to play for the reward
-Config.Minutes = 59 -- do not touch (I don't even know why this is here :/)
-
-Config.VehicleReward = "zentorno" -- vehicle will be spawned in the garage (owned_vehicles)
