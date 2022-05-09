744c8f5134c072bb4697ec76c31fbca71079f932
Tinky <48660658+Tinky124@users.noreply.github.com>
2022-05-09 15:55:28 +0100

    Use math.random for giving player money..

--- ESX/id_playtimereward/shared/config.lua
+++ ESX/id_playtimereward/shared/config.lua
@@ -4,7 +4,7 @@ GlobalState.Minutes = 59 -- do not touch (I don't even know why this is here :/)
 GlobalState.Reward = "vehicle" -- Can be "item", "vehicle" or "money"
 
 GlobalState.VehicleReward = "zentorno" -- vehicle will be spawned in the garage (owned_vehicles) (only if GlobalState.Reward is "vehicle")
-GlobalState.MoneyReward = 10000 -- amount of money that will be given as a reward (only if GlobalState.Reward is "money")
+GlobalState.MoneyReward = math.random(5000, 10000) -- amount of money that will be given as a reward (only if GlobalState.Reward is "money")
 GlobalState.ItemReward = "bread" -- item that will be given as a reward (only if GlobalState.Reward is "item")
 GlobalState.ItemRewardCount = 1 -- count of items that will be given as a reward (only if GlobalState.Reward is "item")
 
